4e4efb8e6b7ea907bd2c7af034e8c3070f026024
Miranda Ku <miranda.ku.paly@gmail.com>
2018-04-27 19:44:54 -0700

    dummy layout

---
+++ Beat Smash/Assets/Scripts/InputReaction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputReaction : MonoBehaviour {
+
+    public string inputKey;
+    private bool isActive;
+
+	// Use this for initialization
+	void Start () {
+        isActive = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(inputKey))
+        {
+            transform.GetComponent<SpriteRenderer>().color = new Color32(255, 0, 0, 255);
+            isActive = true;
+        }
+        if (Input.GetKeyUp(inputKey))
+        {
+            transform.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+            isActive = false;
+        }
+    }
+}
