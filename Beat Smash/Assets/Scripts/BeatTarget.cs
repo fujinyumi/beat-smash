2fa6c3995dcb2b6212cdacd9eca20e88d96a5601
Miranda Ku <miranda.ku.paly@gmail.com>
2018-04-30 15:22:19 -0700

    initial target prefab, overarching script

---
+++ Beat Smash/Assets/Scripts/BeatTarget.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BeatType { Hit, Held };
+
+public class BeatTarget : MonoBehaviour {
+
+    private Lane m_lane;
+    private BeatType m_type;
+    private int m_offset;
+
+    public static Object prototype;
+
+    public void SetLane(Lane lane) { m_lane = lane; }
+    public void SetType(BeatType type) { m_type = type; }
+    public void SetOffset(int offset) { m_offset = offset; }
+
+    public static BeatTarget Create(Lane lane, BeatType type, int offset)
+    {
+        BeatTarget newTarget = Instantiate(prototype) as BeatTarget;
+        newTarget.SetLane(lane);
+        newTarget.SetType(type);
+        newTarget.SetOffset(offset);
+        return newTarget;
+    }
+
+	// Use this for initialization
+	void Start () {
+        prototype = Resources.Load("Prefabs/BeatTarget");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void BeginDescent()
+    {
+    }
+}
---
+++ Beat Smash/Assets/Scripts/Onload.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Lane { D, F, Space, J, K };
+
+public class Onload : MonoBehaviour {
+
+    //hit intervals, in milliseconds - may need to be tweaked later when playtested
+    public const int INTERVAL_GREAT = 10;
+    public const int INTERVAL_GOOD = 20;
+    public const int INTERVAL_BAD = 30;
+
+    private int score;
+    private int totalGreats;
+    private int totalGoods;
+    private int totalBads;
+    private int totalMisses;
+
+    public int getScore() { return score; }
+    public int getGreats() { return totalGreats; }
+    public int getGoods() { return totalGoods; }
+    public int getBads() { return totalBads; }
+    public int getMisses() { return totalMisses; }
+
+    // Use this for initialization
+    void Start () {
+        score = totalGreats = totalGoods = totalBads = totalMisses = 0;
+
+        //perform preprocessing
+        /* i'm going to leave this empty for now */
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/* overall controller which recognizes if notes have been hit?
+         * we need to decide whether or not we want every single note object to be created during the preprocessing phase,
+         * or if they should be created dynamically.
+         * As a result, we need to deide whether this overall controller script handles the timing events, or
+         * if the beats themselves handle their own timing and subsequent destruction. 
+         */
+	}
+}
