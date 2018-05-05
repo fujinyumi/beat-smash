7420d31b564c47892ba893952342bb46c05c9874
smchiang <smchiang@ucla.edu>
2018-05-04 21:21:11 -0700

    Health bar added

--- Beat Smash/Assets/Scripts/ScoreDisplay.cs
+++ Beat Smash/Assets/Scripts/ScoreDisplay.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScoreDisplay : MonoBehaviour {
+    private int score;
 
 	// Use this for initialization
 	void Start () {
-		
+        score = 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    public void GetScore()
+    {
+        return score;
+    }
+
+    public void UpdateScore(int increase)
+    {
+        score += increase;
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
