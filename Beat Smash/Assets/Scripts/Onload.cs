d22a7905e355f66bc5e87d866cc09ef8e002d3fa
Miranda Ku <miranda.ku.paly@gmail.com>
2018-05-05 22:39:40 -0700

    Revert "Merge pull request #1 from fujinyumi/fileio"
    This reverts commit 7681691d95507cabfb09055a0b89f62d596add0c, reversing
    changes made to a4cb08ee5b068ec71a42916ca25daf046796220a.

--- Beat Smash/Assets/Scripts/Onload.cs
+++ Beat Smash/Assets/Scripts/Onload.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
 
-public enum Lane { D, F, Space, J, K, UnInit };
+public enum Lane { D, F, Space, J, K };
 public enum BeatType { Hit, Held };
 
 public class Onload : MonoBehaviour {
@@ -56,8 +55,6 @@ public class Onload : MonoBehaviour {
     // Use this for initialization
     void Start () {
         //member variable initialization
-        //upcomingBeats = map of beats by their timing given by beatmap file
-        //value of map is a List of the beats in order to recognize two beats at once
         score = totalGreats = totalGoods = totalBads = totalMisses = 0;
         upcomingBeats = new SortedDictionary<int, List<BeatInfo>>();
         upcomingBeatsEnumerator = upcomingBeats.GetEnumerator();
@@ -65,88 +62,12 @@ public class Onload : MonoBehaviour {
         upcomingBeatsEnumerator.MoveNext();
 
         /* FOR MICHELLE AND OTHER PREPROCESSORS
-        * Insert preprocessing script here.
-        * INPUT: Read the beatmap file in (using System.IO.File?)
-        * OUTPUT: Using an ordered map (implemented as a red-black tree, so O(logn) access, search, insertion, deletion)
-        * of type < int, List<BeatInfo> > where int corresponds to the time offset (in ms) and BeatInfo is a class containing target info, 
-        * process the beatmap file and consolidate beats with the same ms offset into the List of each key-value pair
-        */
-
-        string filePath = System.IO.Path.GetFullPath("Test.btmp");
-
-        using(var reader = new StreamReader(filePath))
-        {
-            /** REMEMBER TO REMOVE CONSOLE.WRITELINE'S AFTER TESTING **/
-
-            var firstLine = 0;
-            while (!reader.EndOfStream)
-            {
-                var line = reader.ReadLine();
-                var values = line.Split(',');
-
-                // If the firstLine has already been read, start processing values
-                if(firstLine == 1){
-            
-                    // ** Beat Timestamp ** 
-                    int beat_timestamp = -1;
-                    if (Int32.TryParse(values[0], out beat_timestamp))
-                        Console.WriteLine(beat_timestamp);
-                    else
-                        Console.WriteLine("beat_timestamp could not be parsed.");
-
-                    // char laneType = values[1];
-                    // ** Beat Lane **     
-                    Lane beat_lane = Lane.values[1];
-
-                    // ** Beat Type ** 
-                    int beat_type = -1;
-                    if (Int32.TryParse(values[2], out beat_type))
-                        Console.WriteLine(beat_type);
-                    else
-                        Console.WriteLine("beat_type could not be parsed.");
-
-                    // ** Beat Duration ** 
-                    int beat_duration = 1;
-                    if(values.Length > 3) {
-                        beat_duration = values[3];
-
-                        if (Int32.TryParse(values[0], out beat_timestamp))
-                            Console.WriteLine(beat_timestamp);
-                        else
-                            Console.WriteLine("beat_duration could not be parsed.");
-                    }
-
-                    var newBeat = new BeatInfo(beat_timestamp, beat_lane, beat_type, beat_duration);
-
-                    //the set of beats for each time stamp
-                    List<BeatInfo> beatSet = new List<BeatInfo>();
-                    if(upcomingBeats.ContainsKey(beat_timestamp)){
-                        
-                        if (upcomingBeats.TryGetValue(beat_timestamp, out beatSet))
-                        {
-                            beatSet.Add(newBeat);
-                            upcomingBeats[beat_timestamp] = beatSet;
-                            Console.WriteLine("two notes");
-
-                            // Maybe all of this (within TryGetValue if statement)
-                            // can be shortened down to just: 
-                            // upcomingBeats[beat_timestamp].Add(newBeat);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Value is not found.");
-                        }
-                    }
-                    else {
-                        
-                        beatSet.Add(newBeat);
-                        upcomingBeats.Add(beat_timestamp, beatSet);
-                    }
-                }
-                else { firstLine = 1; }
-            }
-
-        }
+         * Insert preprocessing script here.
+         * INPUT: Read the beatmap file in (using System.IO.File?)
+         * OUTPUT: Using an ordered map (implemented as a red-black tree, so O(logn) access, search, insertion, deletion)
+         * of type < int, List<BeatInfo> > where int corresponds to the time offset (in ms) and BeatInfo is a class containing target info, 
+         * process the beatmap file and consolidate beats with the same ms offset into the List of each key-value pair
+         */
 
         /* TO EVENTUALLY REMOVE: HARD CODED DATA */
         List<BeatInfo> listToInsert = new List<BeatInfo>();
