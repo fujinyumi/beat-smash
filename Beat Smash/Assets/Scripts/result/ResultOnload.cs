b9288aba82c703f9796f9d9a233d783e90533584
stpotato <sctom@ucla.edu>
2018-05-15 19:35:50 -0700

    change folder structure

---
+++ Beat Smash/Assets/Scripts/result/ResultOnload.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ResultOnload : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+
+        // TODO: change screen to menu 
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            SceneManager.LoadScene("gamescreen");
+        }
+    }
+
+
+}
