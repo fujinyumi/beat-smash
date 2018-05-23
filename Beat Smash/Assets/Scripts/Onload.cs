a107b5f1d62acd406ade8ef70306e4a692b1d739
mtran26 <21990040+mtran26@users.noreply.github.com>
2018-05-22 23:06:55 -0700

    Moved preprocessing code
    moved preprocessing code to menu-screen code

--- Beat Smash/Assets/Scripts/Onload.cs
+++ Beat Smash/Assets/Scripts/Onload.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public enum Lane { D, F, Space, J, K };
-public enum BeatType { Hit, Held };
+public enum Lane { D, F, Space, J, K, UnInit };
+public enum BeatType { Hit, Held, UnInit };
 
 public class Onload : MonoBehaviour {
 
@@ -84,9 +85,125 @@ public class Onload : MonoBehaviour {
          * process the beatmap file and consolidate beats with the same ms offset into the List of each key-value pair
          */
 
+         /* Current - load file from a set path
+           TO DO: 
+             - Load file from a directory; user specified file name
+             OR 
+             - Load file from any directory that the user specifies
+        */
+        string filePath = System.IO.Path.GetFullPath("Assets/Resources/Beatmaps/Test2.btmp");
+        
+        using(var reader = new StreamReader(filePath))
+        {
+            /** REMEMBER TO REMOVE Debug.Log'S AFTER TESTING **/
+
+            /** THE BEATMAP FILE FORMAT IS: 
+                    bpm 
+                    timestamp, lane, beat type, duration
+                    values[0], [1],  [2],       [3]
+            **/
+
+            var firstLine = 0;
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                // If the firstLine has already been read, start processing values
+                if(firstLine == 1){
+            
+                    // ** Beat Timestamp ** 
+                    int beat_timestamp = -1;
+                    if (int.TryParse(values[0], out beat_timestamp))
+                        Debug.Log(beat_timestamp);
+                    else
+                        Debug.Log("beat_timestamp could not be parsed.");
+
+                    
+                    // ** Beat Lane **     
+                    Lane beat_lane = Lane.UnInit;
+                    string raw_beat_lane = values[1];
+                    switch (raw_beat_lane)
+                    {
+                        case "D": beat_lane = Lane.D;
+                            break;
+                        case "F": beat_lane = Lane.F;
+                            break;
+                        case "Space": beat_lane = Lane.Space;
+                            break;
+                        case "J": beat_lane = Lane.J;
+                            break;
+                        case "K": beat_lane = Lane.K;
+                            break;
+                        default:
+                            Debug.Log("No such key lane.");
+                            break;
+                    }
+
+
+                    // ** Beat Type ** 
+                    BeatType beat_type = BeatType.UnInit;
+                    string raw_beat_type = values[2];
+
+                    switch(raw_beat_type){
+                        case "0": beat_type = BeatType.Hit;
+                            break;
+                        case "1": beat_type = BeatType.Held;
+                            break;
+                        default: 
+                            Debug.Log("No such beat type.");
+                            break;
+                    }
+
+                    var newBeat = new BeatInfo(beat_lane, beat_type, beat_timestamp);
+
+                    // ** Beat Duration ** 
+                    int beat_duration = -1;
+                    if(values.Length > 3) {
+                        if (int.TryParse(values[3], out beat_duration)){
+                            Debug.Log("beat_duration = ");
+                            Debug.Log(beat_duration);
+                            var tmpBeat = new BeatInfo(beat_lane, beat_type, beat_timestamp, beat_duration);
+                            newBeat = tmpBeat; 
+                        }
+                        else
+                            Debug.Log("beat_duration could not be parsed.");
+                    }
+
+                    
+
+                    //the set of beats for each time stamp
+                    List<BeatInfo> beatSet = new List<BeatInfo>();
+                    if(upcomingBeats.ContainsKey(beat_timestamp)){
+                        
+                        if (upcomingBeats.TryGetValue(beat_timestamp, out beatSet))
+                        {
+                            beatSet.Add(newBeat);
+                            upcomingBeats[beat_timestamp] = beatSet;
+                            Debug.Log("two notes");
+
+                            // Maybe all of this (within TryGetValue if statement)
+                            // can be shortened down to just: 
+                            // upcomingBeats[beat_timestamp].Add(newBeat);
+                        }
+                        else
+                        {
+                            Debug.Log("Value is not found.");
+                        }
+                    }
+                    else {                        
+                        beatSet.Add(newBeat);
+                        upcomingBeats.Add(beat_timestamp, beatSet);
+                    }
+                }
+                else { firstLine = 1; Debug.Log("First line skipped"); }
+            }
+
+        } // end of StreamReader code
+
         /* TO EVENTUALLY REMOVE: HARD CODED DATA */
-        TestA testBeatsA = new TestA();
-        testBeatsA.LoadUpcomingBeats(upcomingBeats);
+        // TestA testBeatsA = new TestA();
+        // testBeatsA.LoadUpcomingBeats(upcomingBeats);
 
 
         upcomingBeatsEnumerator = upcomingBeats.GetEnumerator();
