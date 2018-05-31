72f2d5539137a680c9fb83da7aa78b771508ec19
mtran26 <21990040+mtran26@users.noreply.github.com>
2018-05-31 00:01:14 -0700

    Testing automatic load of songs

--- Beat Smash/Assets/Scripts/menu/SongScrollList.cs
+++ Beat Smash/Assets/Scripts/menu/SongScrollList.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -21,19 +22,33 @@ public class SongScrollList : MonoBehaviour {
            Check if audio/beatmaps are both available 
            1) Look into the directory 
            2) Same file names
+           3) Report error if one or the other can't be found
         */
 
+        // Load all song/beatmap information 
+        var beatmaps = Resources.LoadAll("Beatmaps", typeof(TextAsset));
+        foreach (var b in beatmaps)
+        {
+            Debug.Log(b.name);
 
-        SongInfo s0 = new SongInfo("Audio/radio", "Beatmaps/Test", "radio", "0");
-        SongInfo s1 = new SongInfo("Audio/STRM_MAP_C3", "Beatmaps/Test", "STRM_MAP_C3", "1");
-        SongInfo s2 = new SongInfo("Audio/TestA", "Beatmaps/Test", "TestA", "2");
-        SongInfo s3 = new SongInfo("Audio/TestB", "Beatmaps/Test", "TestB", "3");
-        SongInfo s4 = new SongInfo("Audio/delilah", "Beatmaps/delilah", "Delilah", "4");
-        songList.Add(s0);
-        songList.Add(s1);
-        songList.Add(s2);
-        songList.Add(s3);
-        songList.Add(s4);
+            var song_name = b.name;
+            var audio_path = "Audio/" + b.name;
+            var btmp_path = "Beatmaps/" + b.name;
+            Debug.Log(File.Exists("C:/Users/miche/Documents/GitHub/beat-smash/Beat Smash/Assets/Resources/Beatmaps/delilah.txt"));
+            SongInfo song = new SongInfo(audio_path, btmp_path, song_name, "0");
+            songList.Add(song);
+        }
+
+        // SongInfo s0 = new SongInfo("Audio/radio", "Beatmaps/radio", "radio", "0");
+        // SongInfo s1 = new SongInfo("Audio/STRM_MAP_C3", "Beatmaps/Test", "STRM_MAP_C3", "1");
+        // SongInfo s2 = new SongInfo("Audio/TestA", "Beatmaps/Test", "TestA", "2");
+        // SongInfo s3 = new SongInfo("Audio/TestB", "Beatmaps/Test", "TestB", "3");
+        // SongInfo s4 = new SongInfo("Audio/delilah", "Beatmaps/delilah", "Delilah", "4");
+        // songList.Add(s0);
+        // songList.Add(s1);
+        // songList.Add(s2);
+        // songList.Add(s3);
+        // songList.Add(s4);
         AddButtons();
     }
     
