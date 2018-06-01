317b23229fbbfefa20612644593c442eccfb0bed
stpotato <sctom@ucla.edu>
2018-05-31 19:33:45 -0700

    back button

---
+++ Beat Smash/Assets/BackButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BackButton : MonoBehaviour {
+
+    public Button button;
+    public GameObject popWindow;
+
+    // Use this for initialization
+    void Start()
+    {
+        button.onClick.AddListener(HidePopUp);
+    }
+
+    public void HidePopUp()
+    {
+        popWindow.SetActive(false);
+    }
+}
