db9300a8d62db95580973efdfe56973c88acd5d6
smchiang <smchiang@ucla.edu>
2018-06-06 02:08:45 -0700

    Title graphic added and title scene reorganized

---
+++ Beat Smash/Assets/Scripts/Title/Floating.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+float originalY;
+
+private float floatStrength;
+
+public class Floating : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.originalY = this.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x,
+            originalY + ((float)Math.Sin(Time.time) * floatStrength),
+            transform.position.z);
+    }
+}
