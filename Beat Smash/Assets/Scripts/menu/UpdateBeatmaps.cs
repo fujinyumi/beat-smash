28354970d5c4be73e22602d6d82cda4aaf1a4547
stpotato <sctom@ucla.edu>
2018-06-08 01:49:11 -0700

    I/O exception on upload

--- Beat Smash/Assets/Scripts/menu/UpdateBeatmaps.cs
+++ Beat Smash/Assets/Scripts/menu/UpdateBeatmaps.cs
@@ -28,37 +28,44 @@ public class UpdateBeatmaps : MonoBehaviour {
         bool songExists = File.Exists(songPath);
         bool beatmapExists = File.Exists(beatmapPath);
 
-        if (songExists && beatmapExists && title != "")
+        try
         {
-            // copy beatmaps and audio files to resource folder
-            FileUtil.CopyFileOrDirectory(songPath, "Assets/Resources/Audio/" + title + ".wav");
-            FileUtil.CopyFileOrDirectory(beatmapPath, "Assets/Resources/Beatmaps/" + title + ".txt");
+            if (songExists && beatmapExists && title != "")
+            {
+                // copy beatmaps and audio files to resource folder
+                FileUtil.CopyFileOrDirectory(songPath, "Assets/Resources/Audio/" + title + ".wav");
+                FileUtil.CopyFileOrDirectory(beatmapPath, "Assets/Resources/Beatmaps/" + title + ".txt");
 
-            // Refresh asset folder for Unity
-            AssetDatabase.Refresh();
+                // Refresh asset folder for Unity
+                AssetDatabase.Refresh();
 
-            // update buttons on screen
-            Debug.Log("new song info");
-            string newSongPath = "Audio/" + title;
-            string newBeatmapPath = "Beatmaps/" + title;
-            SongInfo newSongInfo = new SongInfo(newSongPath, newBeatmapPath, title, "length not added");
-            scrollList.AddNewSong(newSongInfo);
+                // update buttons on screen
+                Debug.Log("new song info");
+                string newSongPath = "Audio/" + title;
+                string newBeatmapPath = "Beatmaps/" + title;
+                SongInfo newSongInfo = new SongInfo(newSongPath, newBeatmapPath, title, "length not added");
+                scrollList.AddNewSong(newSongInfo);
 
-            //close window and reset
-            ResetWindow();
-            popWindow.SetActive(false);
+                //close window and reset
+                ResetWindow();
+                popWindow.SetActive(false);
+            }
+            else if (!songExists)
+            {
+                notice.text = "Song file does not exist!";
+            }
+            else if (!beatmapExists)
+            {
+                notice.text = "Beatmap file does not exist!";
+            }
+            else if (title == "")
+            {
+                notice.text = "Please enter title";
+            }
         }
-        else if (!songExists)
+        catch (System.IO.IOException)
         {
-            notice.text = "Song file does not exist!";
-        }
-        else if (!beatmapExists)
-        {
-            notice.text = "Beatmap file does not exist!";
-        }
-        else if (title == "")
-        {
-            notice.text = "Please enter title";
+            notice.text = "Cannot copy files!";
         }
     }
 
